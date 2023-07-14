2450e32b582511b72a2452c10487e87d337b6678
jamorr <Morris.Jack@outlook.com>
2023-07-14 11:48:20 -0400

    fixed file names to comply with instructions

---
+++ scripts/fifteen.js
@@ -0,0 +1,29 @@
+// handles player click on board
+// finds tile clicked using target
+// returns tile if found or null if not
+function clickHandler(event) {}
+class Tile extends HTMLDivElement {
+  constructor() {}
+  render(parentElement) {}
+}
+class Board {
+  // initialize new board of given size
+  constructor(size) {}
+  // shuffle board before play begins
+  shuffle() {}
+  // solve the board for the player
+  solve() {}
+}
+class GameLogic {
+  // initializes all instance variables
+  constructor() {}
+
+  // new game
+  initGame() {}
+
+  // end game
+  endGame() {}
+
+  // make move
+  movePiece() {}
+}
